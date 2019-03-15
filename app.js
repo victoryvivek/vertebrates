820d1d9a8431fe68b30dcc7ad28d6f5e692def2d
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2019-03-15 10:32:26 +0530

    form Working

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const app = express();
 
 const amphibianRoutes = require('./routes/amphibians');
+const fishRoutes=require('./routes/fish');
 const indexRoutes = require('./routes/index');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(indexRoutes);
+
 app.use(amphibianRoutes);
+app.use(fishRoutes.router);
+app.use(indexRoutes);
 app.listen(3000);
--- routes/amphibians.js
+++ routes/amphibians.js
@@ -5,8 +5,5 @@ const router = express.Router();
 router.get('/amphibians', (req, res, next) => {
     res.sendFile(path.join(__dirname, "../static/amphibians.html"));
 });
-router.get('/fish', (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../static/fish.html"));
-});
 
 module.exports = router;
